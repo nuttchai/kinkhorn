ffaaccc4501b2fe536b21ea4459f3ce8835b0b25
Visarut Techatanchai <61011358@kmitl.ac.th>
2021-03-15 23:24:13 +0700

    Add login

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
+import Home from './Pages/Home';
+import { BrowserRouter, Route, Switch, RouteComponentProps} from 'react-router-dom';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route path='/oauth/login' />
+      </Switch>
     </div>
   );
 }
